0ed374622ecb170a6988bbd4a6e6b38bf747ad00
wcappel <90431554+wcappel@users.noreply.github.com>
2021-11-20 14:51:08 -0800

    Delete Test.py

--- Test.py
+++
@@ -1,8 +0,0 @@
-import torch
-import random
-from pathlib import Path
-from transformers import BertTokenizer, BertModel, BertForNextSentencePrediction
-from transformers import Trainer, TrainingArguments
-
-# bert_model = BertForNextSentencePrediction.from_pretrained('bert-base-uncased',output_hidden_states=True)
-
